334b313ad377d2028b4e3e608168d7ed90422c36
Talia Bird <minniebird00@gmail.com>
2022-03-26 12:23:09 -0600

    Adding lab details

--- back-end/server.js
+++ back-end/server.js
@@ -10,9 +10,106 @@ app.use(bodyParser.urlencoded({
 const mongoose = require('mongoose');
 
 // connect to the database
-mongoose.connect('mongodb://localhost:27017/museum', {
+mongoose.connect('mongodb://127.0.0.1:27017/museum', {
   useNewUrlParser: true
 });
 
 
-app.listen(3000, () => console.log('Server listening on port 3000!'));
+// Configure multer so that it will upload to '../front-end/public/images'
+const multer = require('multer');
+const upload = multer({
+  dest: '../front-end/public/images/',
+  limits: {
+    fileSize: 10000000
+  }
+});
+
+// Create a scheme for items in the museum: a title and a path to an image.
+const itemSchema = new mongoose.Schema({
+  title: String,
+  path: String,
+  description: String,
+});
+
+// Create a model for items in the museum.
+const Item = mongoose.model('Item', itemSchema);
+
+// Upload a photo. Uses the multer middleware for the upload and then returns
+// the path where the photo is stored in the file system.
+app.post('/api/photos', upload.single('photo'), async (req, res) => {
+  console.log('api/photos ', req.file);
+  // Just a safety check
+  if (!req.file) {
+    return res.sendStatus(400);
+  }
+  res.send({
+    path: "/images/" + req.file.filename
+  });
+});
+
+// Create a new item in the museum: takes a title and a path to an image.
+app.post('/api/items', async (req, res) => {
+  console.log('api/items ', req.body.title);
+  const item = new Item({
+    title: req.body.title,
+    path: req.body.path,
+    description: req.body.description,
+  });
+  try {
+    await item.save();
+    res.send(item);
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(500);
+  }
+});
+
+// Get a list of all of the items in the museum.
+app.get('/api/items', async (req, res) => {
+  try {
+    let items = await Item.find();
+    res.send(items);
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(500);
+  }
+});
+
+// Deleting an item from the list, works!
+app.delete('/api/items/:id', async (req, res) => {
+  console.log('Deleting ', req.params);
+  try {
+    await Item.deleteOne({
+      _id: req.params.id
+    });
+    res.sendStatus(200);
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(500);
+  }
+});
+
+// Editing an item.
+app.put('/api/items/:id', async (req, res) => {
+  console.log('Editing an item: ', req.params.id)
+  try {
+    let item = await Item.findOne({
+      _id: req.params.id
+    });
+    console.log('Item before edit:', item.title, ' and ', item.description);
+
+    item.title = req.body.title;
+    item.description = req.body.description;
+    item.save();
+    console.log('Item after edit:', item.title, ' and ', item.description);
+
+    res.sendStatus(200);
+
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(500);
+  }
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Server listening on port ${port}!`));
